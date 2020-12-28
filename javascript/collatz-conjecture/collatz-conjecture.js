585dda5996fed9535bd46c45473baca339007f2c
John Young <john.young@thinkcompany.com>
2020-12-28 15:23:34 +0000

    First solution to Collatz Conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,8 +1,21 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const steps = (input) => {
+  let currentNum = input;
+  let count = 0;
 
-export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
+  // Reject zero and negative values
+  if (currentNum <= 0) {
+    throw ('Only positive numbers are allowed');
+  }
+
+  // Perform operations until currentNum is 1
+  while (currentNum > 1) {
+    if (currentNum % 2 === 0 ) { // currentNum is even
+      currentNum = currentNum / 2;
+    } else { // currentNum is odd
+      currentNum = ( 3 * currentNum ) + 1;
+    }
+    count++;
+  }
+
+  return count;
 };
