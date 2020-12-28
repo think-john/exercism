06759a9292d6206d4095560ebd1dd41ce1849725
John Young <john.young@thinkcompany.com>
2020-12-28 21:00:19 +0000

    Updated solution to resistor-color-duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,18 +1,9 @@
-const COLORS = [
-  "black",
-  "brown",
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "violet",
-  "grey",
-  "white"
-];
+const COLORS = "black brown red orange yellow green blue violet grey white".split(' ')
 
-export const value = (colors) => {
+export const decodedValue = (colors) => {
   return colors
-          .map(i => COLORS.indexOf(i))
-          .join('') * 1;
+    .map( i => COLORS.indexOf(i) ) // ['brown','green','orange'] => ['1','5','3']
+    .join('')                      // ['1','5','3'] => '153'
+    .substring(0,2)                // '153' => '15' 
+    * 1;                           // '15' => 15
 }
