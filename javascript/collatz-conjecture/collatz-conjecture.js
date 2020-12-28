ecf32ac2bf4f8dfc0e139d5f1aeea510cb3e3923
John Young <john.young@thinkcompany.com>
2020-12-28 15:33:03 +0000

    Now using recursion

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,15 +1,7 @@
 export const steps = (n, count = 0) => {
 
   if (n <= 0) throw 'Only positive numbers are allowed'
+  if (n === 1) return count;
+  return steps( n % 2 === 0 ? n / 2 : n * 3 + 1 , count + 1)
 
-  while (n > 1) {
-    if (n % 2 === 0 ) { 
-      n = n / 2;
-    } else {
-      n = ( 3 * n ) + 1;
-    }
-    count++;
-  }
-
-  return count;
 };
