ea9b43ecdaf6fb976eccff67e75e0047f8249f81
John Young <john.young@thinkcompany.com>
2021-01-03 15:34:21 +0000

    New version of meetup.js that passes all tests.

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,27 +1,26 @@
-// expected input: meetup(2013, 5, 'teenth', 'Monday')
-
-const ORDINALS = "first second third fourth fifth".split(' ');
-const DAYNAMES = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(' ');
-
-let days = {
-  Sunday: [],
-  Monday: [],
-  Tuesday: [],
-  Wednesday: [],
-  Thursday: [],
-  Friday: [],
-  Saturday: []
-}
 
 export const meetup = (desiredYear, desiredMonth, desiredDescriptor, desiredDayOfWeek) => {
+  // expected input: meetup(2013, 5, 'teenth', 'Monday')
 
   // Months are zero-indexed in Javascript: January is 0, February is 1.
   // Use month: 4 to construct a date in May.
   // https://stackoverflow.com/questions/2552483/why-does-the-month-argument-range-from-0-to-11-in-javascripts-date-constructor
   const javascriptIndexMonth = (desiredMonth - 1) 
- 
+
+  const ORDINALS = "first second third fourth fifth".split(' ');
+  const DAYNAMES = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(' ');
+
+  const days = {
+    Sunday: [],
+    Monday: [],
+    Tuesday: [],
+    Wednesday: [],
+    Thursday: [],
+    Friday: [],
+    Saturday: []
+  };
+
   // Populate the days array with all the days from desiredMonth and desiredYear
-  //console.log(`let currentDate = new Date(${desiredYear}, ${javascriptIndexMonth}, 1);`)
   let currentDate = new Date(desiredYear, javascriptIndexMonth, 1);
 
   do {
@@ -35,8 +34,8 @@ export const meetup = (desiredYear, desiredMonth, desiredDescriptor, desiredDayO
   // Get an array of (for instance) Mondays
   // days['Monday'] in May 2013 would be [6,13,20,27]
   var candidateDates = days[desiredDayOfWeek];
-  var candidateDay = ''
 
+  var candidateDay = ''
   // Use the descriptor to decide the day of the month
   switch (desiredDescriptor) {
     case 'teenth':
